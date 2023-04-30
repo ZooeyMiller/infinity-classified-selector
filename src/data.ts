cc6bb8be9d209680a7970dafcaf88e20c89dc1ed
zooey <zooey@oldstlabs.com>
2023-04-30 12:42:32 +0100

    add main app functionality

---
+++ src/data.ts
@@ -0,0 +1,162 @@
+const greenCard1 = new URL("../../assets/green-capture.png", import.meta.url)
+  .href;
+const greenCard2 = new URL("../../assets/green-data-scan.png", import.meta.url)
+  .href;
+const greenCard3 = new URL(
+  "../../assets/green-experimental-drug.png",
+  import.meta.url
+).href;
+const greenCard4 = new URL(
+  "../../assets/green-extreme-prejudice.png",
+  import.meta.url
+).href;
+const greenCard5 = new URL("../../assets/green-follow-up.png", import.meta.url)
+  .href;
+const greenCard6 = new URL(
+  "../../assets/green-hvt-designation.png",
+  import.meta.url
+).href;
+const greenCard7 = new URL(
+  "../../assets/green-hvt-espionage.png",
+  import.meta.url
+).href;
+const greenCard8 = new URL(
+  "../../assets/green-hvt-identity-check.png",
+  import.meta.url
+).href;
+const greenCard9 = new URL(
+  "../../assets/green-hvt-inoculation.png",
+  import.meta.url
+).href;
+const greenCard10 = new URL(
+  "../../assets/green-hvt-kidnapping.png",
+  import.meta.url
+).href;
+const greenCard11 = new URL(
+  "../../assets/green-hvt-retroengineering.png",
+  import.meta.url
+).href;
+const greenCard12 = new URL(
+  "../../assets/green-in-extremis-recovery.png",
+  import.meta.url
+).href;
+const greenCard13 = new URL("../../assets/green-mapping.png", import.meta.url)
+  .href;
+const greenCard14 = new URL(
+  "../../assets/green-nanoespionage.png",
+  import.meta.url
+).href;
+const greenCard15 = new URL(
+  "../../assets/green-net-undermine.png",
+  import.meta.url
+).href;
+const greenCard16 = new URL("../../assets/green-predator.png", import.meta.url)
+  .href;
+const greenCard17 = new URL("../../assets/green-rescue.png", import.meta.url)
+  .href;
+const greenCard18 = new URL("../../assets/green-sabotage.png", import.meta.url)
+  .href;
+const greenCard19 = new URL("../../assets/green-telemetry.png", import.meta.url)
+  .href;
+const greenCard20 = new URL("../../assets/green-test-run.png", import.meta.url)
+  .href;
+const redCard1 = new URL("../../assets/red-capture.png", import.meta.url).href;
+const redCard2 = new URL("../../assets/red-data-scan.png", import.meta.url)
+  .href;
+const redCard3 = new URL(
+  "../../assets/red-experimental-drug.png",
+  import.meta.url
+).href;
+const redCard4 = new URL(
+  "../../assets/red-extreme-prejudice.png",
+  import.meta.url
+).href;
+const redCard5 = new URL("../../assets/red-follow-up.png", import.meta.url)
+  .href;
+const redCard6 = new URL(
+  "../../assets/red-hvt-designation.png",
+  import.meta.url
+).href;
+const redCard7 = new URL("../../assets/red-hvt-espionage.png", import.meta.url)
+  .href;
+const redCard8 = new URL(
+  "../../assets/red-hvt-identity-check.png",
+  import.meta.url
+).href;
+const redCard9 = new URL(
+  "../../assets/red-hvt-inoculation.png",
+  import.meta.url
+).href;
+const redCard10 = new URL(
+  "../../assets/red-hvt-kidnapping.png",
+  import.meta.url
+).href;
+const redCard11 = new URL(
+  "../../assets/red-hvt-retroengineering.png",
+  import.meta.url
+).href;
+const redCard12 = new URL(
+  "../../assets/red-in-extremis-recovery.png",
+  import.meta.url
+).href;
+const redCard13 = new URL("../../assets/red-mapping.png", import.meta.url).href;
+const redCard14 = new URL("../../assets/red-nanoespionage.png", import.meta.url)
+  .href;
+const redCard15 = new URL("../../assets/red-net-undermine.png", import.meta.url)
+  .href;
+const redCard16 = new URL("../../assets/red-predator.png", import.meta.url)
+  .href;
+const redCard17 = new URL("../../assets/red-rescue.png", import.meta.url).href;
+const redCard18 = new URL("../../assets/red-sabotage.png", import.meta.url)
+  .href;
+const redCard19 = new URL("../../assets/red-telemetry.png", import.meta.url)
+  .href;
+const redCard20 = new URL("../../assets/red-test-run.png", import.meta.url)
+  .href;
+
+export const greenDeck = [
+  greenCard1,
+  greenCard2,
+  greenCard3,
+  greenCard4,
+  greenCard5,
+  greenCard6,
+  greenCard7,
+  greenCard8,
+  greenCard9,
+  greenCard10,
+  greenCard11,
+  greenCard12,
+  greenCard13,
+  greenCard14,
+  greenCard15,
+  greenCard16,
+  greenCard17,
+  greenCard18,
+  greenCard19,
+  greenCard20
+];
+export const redDeck = [
+  redCard1,
+  redCard2,
+  redCard3,
+  redCard4,
+  redCard5,
+  redCard6,
+  redCard7,
+  redCard8,
+  redCard9,
+  redCard10,
+  redCard11,
+  redCard12,
+  redCard13,
+  redCard14,
+  redCard15,
+  redCard16,
+  redCard17,
+  redCard18,
+  redCard19,
+  redCard20
+];
+
+export const bothDeck = [...greenDeck, ...redDeck];
--- src/infinity-classified-selector.ts
+++ src/infinity-classified-selector.ts
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from "lit";
 import { property, state, customElement } from "lit/decorators.js";
-
-const logo = new URL("../../assets/open-wc-logo.svg", import.meta.url).href;
+import { greenDeck, redDeck, bothDeck } from "./data";
+import { classMap } from "lit/directives/class-map.js";
 
 enum Section {
   SelectDeck,
@@ -16,11 +16,9 @@ enum Deck {
   Both
 }
 
-enum Card {
-  TODO
-}
+type Card = string;
 
-const shuffle = arr => {
+const shuffle = <T>(arr: Array<T>): Array<T> => {
   const res = arr.map(value => ({ value, order: Math.random() }));
 
   res.sort((o1, o2) =>
@@ -34,6 +32,7 @@ const shuffle = arr => {
 export class InfinityClassifiedSelector extends LitElement {
   @state() section: Section = Section.SelectDeck;
   @state() deck: Deck | null = null;
+  @state() drawPile: Array<Card> = [];
   @state() selected: Array<Card> = [];
   @state() rejected: Array<Card> = [];
   @state() completed: Array<Card> = [];
@@ -41,41 +40,146 @@ export class InfinityClassifiedSelector extends LitElement {
 
   renderSection = () => {
     switch (this.section) {
-      case SelectDeck:
+      case Section.SelectDeck:
         return this.renderSelectDeck();
-      case SelectAmount:
+      case Section.SelectAmount:
         return this.renderSelectAmount();
-      case SelectClassifieds:
+      case Section.SelectClassifieds:
         return this.renderSelectClassifieds();
-      case ViewClassifieds:
-        this.renderViewClassifieds();
+      case Section.ViewClassifieds:
+        return this.renderViewClassifieds();
       default:
-        this.renderError();
+        return this.renderError();
     }
   };
 
-  static styles = css``;
+  renderSelectDeck = () => {
+    return html`
+      <button @click=${this.handleSelectDeck(Deck.Green)}>Green</button>
+      <button @click=${this.handleSelectDeck(Deck.Red)}>Red</button>
+      <button @click=${this.handleSelectDeck(Deck.Both)}>Both</button>
+    `;
+  };
+
+  renderSelectAmount = () => {
+    return html`
+      <button @click=${this.handleSelectAmount(1)}>1</button>
+      <button @click=${this.handleSelectAmount(2)}>2</button>
+      <button @click=${this.handleSelectAmount(3)}>3</button>
+      <button @click=${this.handleSelectAmount(4)}>4</button>
+      <button @click=${this.handleSelectAmount(5)}>5</button>
+    `;
+  };
+
+  renderSelectClassifieds = () => {
+    const [one, two, ...rest] = this.drawPile;
+
+    return html`
+      <img src=${one} />
+      <button @click=${this.handleSelectClassifieds(one, two, rest)}>
+        Select
+      </button>
+      <img src=${two} />
+      <button @click=${this.handleSelectClassifieds(two, one, rest)}>
+        Select
+      </button>
+    `;
+  };
+
+  renderViewClassifieds = () => {
+    return this.selected.map(image => {
+      const completed = this.completed.includes(image);
+      console.log(completed);
+
+      return html`<img class=${classMap({ completed })} src=${image} />
+        <button
+          @click=${completed
+            ? this.handleMarkAsIncomplete(image)
+            : this.handleMarkAsComplete(image)}
+        >
+          Mark As ${completed ? "Incomplete" : "Complete"}
+        </button>`;
+    });
+  };
+
+  renderError = () => {
+    return "error has occurred, please reset";
+  };
+
+  setSection = (): void => {
+    if (this.deck === null) {
+      this.section = Section.SelectDeck;
+    }
+    if (this.deck !== null) {
+      this.section = Section.SelectAmount;
+    }
+    if (this.amount > 0) {
+      this.section = Section.SelectClassifieds;
+    }
+    if (this.selected.length === this.amount && this.amount > 0) {
+      this.section = Section.ViewClassifieds;
+    }
+  };
+  static styles = css`
+    .completed {
+      opacity: 0.5;
+    }
+  `;
 
   firstUpdated = (): void => {
     // TODO: localStorage check + load
-  }
+  };
 
   handleSelectDeck = (deck: Deck) => (): void => {
     this.deck = deck;
+
+    switch (deck) {
+      case Deck.Green:
+        this.drawPile = shuffle(greenDeck);
+        break;
+      case Deck.Red:
+        this.drawPile = shuffle(redDeck);
+        break;
+      case Deck.Both:
+        this.drawPile = shuffle(bothDeck);
+        break;
+      default:
+        this.drawPile = [];
+        break;
+    }
+
+    this.setSection();
   };
 
   handleSelectAmount = (amount: number) => (): void => {
     this.amount = amount;
+    this.setSection();
   };
 
-  handleSelectClassifieds = (selected: Card, rejected: Card) => (): void => {
+  handleSelectClassifieds = (
+    selected: Card,
+    rejected: Card,
+    rest: Array<Card>
+  ) => (): void => {
     this.selected = [...this.selected, selected];
     this.rejected = [...this.rejected, rejected];
+    this.drawPile = rest;
+    this.setSection();
     // TODO: localStorage set
   };
 
   handleMarkAsComplete = (card: Card) => (): void => {
     this.completed = [...this.completed, card];
+    // TODO: localStorage set
+  };
+
+  handleMarkAsIncomplete = (card: Card) => (): void => {
+    const idx = this.completed.findIndex(c => c === card);
+
+    this.completed = [
+      ...this.completed.slice(0, idx),
+      ...this.completed.slice(idx + 1)
+    ];
     // TODO: localStorage set
   };
 
@@ -99,6 +203,8 @@ export class InfinityClassifiedSelector extends LitElement {
         <div class="body">
           ${this.renderSection()}
         </div>
+
+        <button @click=${this.handleReset}>Reset</button>
       </main>
     `;
   }
