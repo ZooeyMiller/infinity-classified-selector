7635d288e29338c68b345f6109690e42a35d6fb6
zooey <zooey@oldstlabs.com>
2023-04-30 09:51:55 +0100

    add shuffle and handlers

--- src/infinity-classified-selector.ts
+++ src/infinity-classified-selector.ts
@@ -20,6 +20,16 @@ enum Card {
   TODO
 }
 
+const shuffle = arr => {
+  const res = arr.map(value => ({ value, order: Math.random() }));
+
+  res.sort((o1, o2) =>
+    o1.order > o2.order ? 1 : o1.order < o2.order ? -1 : 0
+  );
+
+  return res.map(({ value }) => value);
+};
+
 @customElement("infinity-classified-selector")
 export class InfinityClassifiedSelector extends LitElement {
   @state() section: Section = Section.SelectDeck;
@@ -27,7 +37,7 @@ export class InfinityClassifiedSelector extends LitElement {
   @state() selected: Array<Card> = [];
   @state() rejected: Array<Card> = [];
   @state() completed: Array<Card> = [];
-  @state() amount: number = 0
+  @state() amount: number = 0;
 
   renderSection = () => {
     switch (this.section) {
@@ -45,6 +55,39 @@ export class InfinityClassifiedSelector extends LitElement {
   };
 
   static styles = css``;
+
+  firstUpdated = (): void => {
+    // TODO: localStorage check + load
+  }
+
+  handleSelectDeck = (deck: Deck) => (): void => {
+    this.deck = deck;
+  };
+
+  handleSelectAmount = (amount: number) => (): void => {
+    this.amount = amount;
+  };
+
+  handleSelectClassifieds = (selected: Card, rejected: Card) => (): void => {
+    this.selected = [...this.selected, selected];
+    this.rejected = [...this.rejected, rejected];
+    // TODO: localStorage set
+  };
+
+  handleMarkAsComplete = (card: Card) => (): void => {
+    this.completed = [...this.completed, card];
+    // TODO: localStorage set
+  };
+
+  handleReset = (): void => {
+    this.section = Section.SelectDeck;
+    this.deck = null;
+    this.selected = [];
+    this.rejected = [];
+    this.completed = [];
+    this.amount = 0;
+    // TODO: localStorage reset
+  };
 
   render() {
     return html`
