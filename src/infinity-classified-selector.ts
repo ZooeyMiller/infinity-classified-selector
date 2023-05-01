4ea5cd07f2e9526a1a8072432f235b1a9a85c7cc
zooey <zooey@oldstlabs.com>
2023-05-01 11:22:40 +0100

    do the css

--- src/infinity-classified-selector.ts
+++ src/infinity-classified-selector.ts
@@ -38,6 +38,22 @@ export class InfinityClassifiedSelector extends LitElement {
   @state() completed: Array<Card> = [];
   @state() amount: number = 0;
 
+  getTitleText = () => {
+    switch (this.section) {
+      case Section.SelectDeck:
+        return "Select classified deck:";
+      case Section.SelectAmount:
+        return "Select number of classifieds:";
+      case Section.SelectClassifieds:
+        return `Select classified${
+          this.amount === 1 ? "" : ` ${this.selected.length + 1}`
+        }:`;
+      case Section.ViewClassifieds:
+      default:
+        return "";
+    }
+  };
+
   renderSection = () => {
     switch (this.section) {
       case Section.SelectDeck:
@@ -55,19 +71,44 @@ export class InfinityClassifiedSelector extends LitElement {
 
   renderSelectDeck = () => {
     return html`
-      <button @click=${this.handleSelectDeck(Deck.Green)}>Green</button>
-      <button @click=${this.handleSelectDeck(Deck.Red)}>Red</button>
-      <button @click=${this.handleSelectDeck(Deck.Both)}>Both</button>
+      <button
+        class="space-bottom green"
+        @click=${this.handleSelectDeck(Deck.Green)}
+      >
+        Green
+      </button>
+      <button
+        class="space-bottom red"
+        @click=${this.handleSelectDeck(Deck.Red)}
+      >
+        Red
+      </button>
+      <button
+        class="space-bottom red-green"
+        @click=${this.handleSelectDeck(Deck.Both)}
+      >
+        Both
+      </button>
     `;
   };
 
   renderSelectAmount = () => {
     return html`
-      <button @click=${this.handleSelectAmount(1)}>1</button>
-      <button @click=${this.handleSelectAmount(2)}>2</button>
-      <button @click=${this.handleSelectAmount(3)}>3</button>
-      <button @click=${this.handleSelectAmount(4)}>4</button>
-      <button @click=${this.handleSelectAmount(5)}>5</button>
+      <button class="space-bottom" @click=${this.handleSelectAmount(1)}>
+        1
+      </button>
+      <button class="space-bottom" @click=${this.handleSelectAmount(2)}>
+        2
+      </button>
+      <button class="space-bottom" @click=${this.handleSelectAmount(3)}>
+        3
+      </button>
+      <button class="space-bottom" @click=${this.handleSelectAmount(4)}>
+        4
+      </button>
+      <button class="space-bottom" @click=${this.handleSelectAmount(5)}>
+        5
+      </button>
     `;
   };
 
@@ -75,11 +116,11 @@ export class InfinityClassifiedSelector extends LitElement {
     const [one, two, ...rest] = this.drawPile;
 
     return html`
-      <img src=${one} />
+      <img class="card" src=${one} />
       <button @click=${this.handleSelectClassifieds(one, two, rest)}>
         Select
       </button>
-      <img src=${two} />
+      <img class="card" src=${two} />
       <button @click=${this.handleSelectClassifieds(two, one, rest)}>
         Select
       </button>
@@ -91,8 +132,12 @@ export class InfinityClassifiedSelector extends LitElement {
       const completed = this.completed.includes(image);
       console.log(completed);
 
-      return html`<img class=${classMap({ completed })} src=${image} />
+      return html`<img
+          class=${classMap({ completed, card: true })}
+          src=${image}
+        />
         <button
+          class="space-bottom"
           @click=${completed
             ? this.handleMarkAsIncomplete(image)
             : this.handleMarkAsComplete(image)}
@@ -120,11 +165,6 @@ export class InfinityClassifiedSelector extends LitElement {
       this.section = Section.ViewClassifieds;
     }
   };
-  static styles = css`
-    .completed {
-      opacity: 0.5;
-    }
-  `;
 
   firstUpdated = (): void => {
     // TODO: localStorage check + load
@@ -165,6 +205,9 @@ export class InfinityClassifiedSelector extends LitElement {
     this.rejected = [...this.rejected, rejected];
     this.drawPile = rest;
     this.setSection();
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
     // TODO: localStorage set
   };
 
@@ -193,18 +236,96 @@ export class InfinityClassifiedSelector extends LitElement {
     // TODO: localStorage reset
   };
 
+  static styles = css`
+    :host {
+    }
+
+    * {
+      box-sizing: border-box;
+      font-family: Helvetica, sans-serif;
+      font-size: 36px;
+    }
+
+    .completed {
+      opacity: 0.5;
+    }
+
+    main {
+      background-color: white;
+      padding: 8px 8px 74px 8px;
+      position: relative;
+      min-height: 100vh;
+    }
+
+    button {
+      width: 100%;
+      text-transform: uppercase;
+      color: white;
+      padding: 8px;
+      border: 1px solid black;
+      border-radius: 4px;
+      background-color: #00008b;
+    }
+
+    .reset {
+      position: absolute;
+      bottom: 8px;
+      width: calc(100% - 16px);
+    }
+
+    .header {
+      background-color: #00008b;
+      margin: -8px -8px 8px -8px;
+
+      color: white;
+      display: flex;
+      padding: 8px;
+    }
+
+    .header-text {
+      font-size: 24px;
+    }
+
+    .green {
+      background-color: yellowgreen;
+    }
+
+    .red {
+      background-color: red;
+    }
+
+    .red-green {
+      background-image: linear-gradient(to right, yellowgreen, red);
+    }
+
+    .space-bottom {
+      margin-bottom: 8px;
+    }
+
+    .card {
+      max-width: 100%;
+    }
+
+    h1 {
+      font-size: 24px;
+    }
+  `;
+
   render() {
     return html`
       <main>
         <div class="header">
-          <span>Infinity Classified Selector</span>
+          <span class="header-text">Infinity Classified Selector</span>
         </div>
 
         <div class="body">
+          ${this.section === Section.ViewClassifieds
+            ? ""
+            : html` <h1>${this.getTitleText()}</h1> `}
           ${this.renderSection()}
         </div>
 
-        <button @click=${this.handleReset}>Reset</button>
+        <button class="reset" @click=${this.handleReset}>Reset</button>
       </main>
     `;
   }
